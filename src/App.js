c9b1aef9da878c82091bd5ac5dee6c7673c523f4
Eko Dwi Saputro <44047301+ekods@users.noreply.github.com>
2021-02-25 10:59:58 +0700

    index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './Header';
+import List from './List';
+import Content from './Content';
+
+
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Header />
+
+
+        <Container>
+          <Row>
+            <Col xs={12} md={3}>
+              <List />
+            </Col>
+            <Col xs={12} md={9}>
+              <Content />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
